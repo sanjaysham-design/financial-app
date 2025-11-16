31996d479ca5eef3ad149e839bb033faee014a6f
SANJAY SHAMDASANI <sanjayshamdasani@MacBook-Pro-24.local>
2025-11-16 13:38:13 -0800

    Deploy updates

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import './App.css';
 import { Newspaper, BarChart3, Target, Search, Loader, Menu, X, Settings } from 'lucide-react';
 import {
   ResponsiveContainer,
